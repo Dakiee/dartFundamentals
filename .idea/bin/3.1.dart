9cc1a6f45b0d8c315bdb4d9be614fbacd542c841
Dakiee <ldavaanyam@gmail.com>
2022-03-16 23:47:26 +0800

    types

---
+++ .idea/bin/3.1.dart
@@ -0,0 +1,22 @@
+void main(){
+  int score = 23;
+  var count = 23;
+  int hexValue = 0xEADEBAEE;
+
+  double percentage=93.4;
+  var percent = 82.5333;
+  double exponents = 1.42e5;
+
+  String name = "Dakie";
+  var company = "Microsoft";
+
+  bool isValid = true;
+  var isAlive = false;
+
+  print (score);
+  print (percentage);
+  print (name);
+  print (isValid);
+
+  //NOTE: All data types in Dart are objects
+}
---
+++ .idea/bin/3.dart
@@ -0,0 +1,5 @@
+void main() {
+  //Trying comments
+  print("Hello World");
+  print(420-69);
+}
